42939802867cf8db21bced9c6e5a6ee5e75d13a8
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-27 00:08:11 -0300

    update lesson
    update lesson

--- functions.js
+++ functions.js
@@ -22,3 +22,55 @@ arr2 = [...arr1];  // Change this line
 
 console.log(arr2);
 //-----------------------------------
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const {today} = HIGH_TEMPERATURES;
+  const {tomorrow} = HIGH_TEMPERATURES;
+  
+  // Only change code above this line
+//-----------------------------------
+const HIGH_TEMPERATURES2 = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+    
+  const {today: highToday} = HIGH_TEMPERATURES2;
+  const {tomorrow: highTomorrow} = HIGH_TEMPERATURES2; 
+  
+  // Only change code above this line
+//-----------------------------------
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+    
+  const {today: { low: lowToday}} = LOCAL_FORECAST;
+  const {today: { high: highToday}} = LOCAL_FORECAST;
+  
+  // Only change code above this line
+//-----------------------------------
+let a = 8, b = 6;
+// Only change code below this line
+[b,a] = [a,b]
+//-----------------------------------
+function removeFirstTwo(list) {
+    const [a,b, ...arr] = list;
+    return arr;
+  }
+  
+  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const sourceWithoutFirstTwo = removeFirstTwo(source);
+  console.log(sourceWithoutFirstTwo)
+//-----------------------------------
