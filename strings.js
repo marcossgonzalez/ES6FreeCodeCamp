ec5ac2409908dcf076db3203dcfce4ccb8bc9e86
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-29 03:08:29 -0300

    Update lesson
    update lesson

--- strings.js
+++ strings.js
@@ -33,5 +33,25 @@ const result = {
     return failureItems;
     
   }
-  
+//----------------------------------- 
   const failuresList = makeList(result.failure);
+
+  const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name,age,gender
+    };
+    // Only change code above this line
+  };
+//-----------------------------------
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear (newGear) {
+      this.gear = newGear;
+    }
+  };
+  // Only change code above this line
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
+//-----------------------------------
