414ce0cf639156b1caf7ae34fbc465ccabdc0405
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-09-29 02:40:10 -0300

    update lesson
    update lesson

--- strings.js
+++ strings.js
@@ -1,1 +1,37 @@
 //-----------------------------------
+/*Um novo recurso do ES6 é o modelo literal. Este é um tipo especial de string que facilita a criação de strings complexas.
+
+Literais de modelo permitem criar strings multilinhas e usar recursos de interpolação de strings para criar strings.
+EX:
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56
+};
+
+const greeting = `(isso é uma crase ou acento grave e não um apostrofo) Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
+
+
+*/
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    let i = 0;
+    let failureItems = [];
+    do {
+      failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+      i++;
+      }while(i<arr.length);
+      console.log(failureItems);
+    // Only change code above this line
+    return failureItems;
+    
+  }
+  
+  const failuresList = makeList(result.failure);
